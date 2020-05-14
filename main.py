828a3b48d2a0cae285d664441053d8d7bc643399
mrborisov <mrborisovv@gmail.com>
2020-05-14 21:25:10 +1000

    mail
    add send mail

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import logging
 import shutil
 
 import config
+import mail_send
 
 
 def copy_date(cur_dir, dst_dir, blok):
     """
     Функция копирования файлов из cur_dir в dst_dir, blok это блок из конфига
+    cur_date: дата в виде строки добавляется к имени каталога назначения копирования
     """
     dt = datetime.datetime.now()
     cur_date = dt.strftime('%Y_%m_%d-%H%M')
@@ -18,15 +20,19 @@ def copy_date(cur_dir, dst_dir, blok):
         logging.info(cur_date + " Start copy" + blok)
     except OSError as why:
         logging.error(cur_date + ' ' + str(why))
+        mail_send.send_err(str(why))
     except IOError as why:
         logging.error(str(why))
+        mail_send.send_err(str(why))
     logging.info(cur_date + " Finish copy" + blok)
 
 
-path = "settings.ini"
-cfg_file = config.get_config(path)
+cfg_file = config.get_config()
+''' 
+Читаем конфиг по секциям, пропускаем секцию mail и делаем копирование
+'''
 for el in cfg_file.sections():
-    if el != 'mail':
-        cur_dir = config.get_setting(path, el, 'cur_dir')
-        dst_dir = config.get_setting(path, el, 'dst_dir')
+    if el != 'mail' and el != 'area':
+        cur_dir = config.get_setting(el, 'cur_dir')
+        dst_dir = config.get_setting(el, 'dst_dir')
         copy_date(cur_dir, dst_dir, el)
